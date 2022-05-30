bc9de6800dbff274968d4a6fcf0f6baaa44030fb
nate mernyk <n8emernyk@gmail.com>
2022-05-30 14:47:40 -0400

    first shot

--- src/components/PuzzleBase.vue
+++ src/components/PuzzleBase.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="puzzle">
-      <puzzle-card/>
       <h3>first card will be here</h3>
+      <puzzle-card :string="currentString" :rules="rulesOptions" @rule="enactRule" />
       <h3>and arrayed beneath it will be the options for the next step based on the rules</h3>
   </div>
 </template>
@@ -13,6 +13,66 @@ export default {
   name: 'PuzzleBase',
   props: {
     
+  },
+  data() {
+    return {
+      currentString: "MI",
+      location: 0,
+      rulesWritten: {first: "If you possess a string whose last letter is I, you may add a U to the end",
+              second: "Mx (x standing in for any sequence of characters following the M and running to the end of the string), you may add Mxx to your collection.",
+              third: "If III occurs in the string, you may make a new string with U in place of III",
+              fourth: "If UU occurs inside one of your strings, you can drop it"},
+      rulesLogic: {first: () => {
+      this.currentString += "U"},
+      second: () => {
+      this.currentString += this.currentString.substring(1, this.currentString.length);},
+      third: () => {
+      this.currentString = this.currentString.substring(0, this.location ) + "U" + this.currentString.substring(this.location + 3, this.currentString.length)},
+      fourth: () => {
+      this.currentString = this.currentString.substring(0, this.location ) + this.currentString.substring(this.location + 2, this.currentString.length)}},
+      rulesOptions: [],
+    }
+  },
+  methods: {
+      whichRulesAreValid(){
+        this.rulesOptions = []
+        if(this.currentString.substring(this.currentString.length - 1, this.currentString.length) == "I"){
+          this.rulesOptions.push(1)
+        }
+        if(this.currentString.length > 1 && this.currentString.substring(0,1) == "M"){
+          this.rulesOptions.push(2)
+        }
+        if(this.currentString.includes("III")){
+          this.iLocation = this.currentString.indexOf("III");
+          this.rulesOptions.unshift(3)
+        }
+        if(this.currentString.includes("UU")){
+          this.uLocation = this.currentString.indexOf("UU");
+          this.rulesOptions.unshift(4)
+        }
+      },
+      enactRule(...args){
+        let rule = args[0];
+        if(args[1] != undefined){
+          this.location = args[1];
+        }
+        if(rule == 1){
+          this.rulesLogic.first()
+        }
+        if(rule == 2){
+          this.rulesLogic.second()
+        }
+        if(rule == 3){
+          this.rulesLogic.third()
+        }
+        if(rule == 4){
+          this.rulesLogic.fourth()
+        }
+        this.whichRulesAreValid();
+      }
+  },
+  created() {
+    this.whichRulesAreValid();
   }
 }
 </script>
--- src/components/PuzzleCard.vue
+++ src/components/PuzzleCard.vue
@@ -1,20 +1,47 @@
 <template>
     <div class="card">
-        <h2 class="card-text">MU</h2>
+        <h2 class="card-text">{{string}}</h2>
+        <div v-for="rule in rules" :key="rule.value">
+      <button @click="handleClick(rule)">{{ruleLegend[rule -1]}}</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'puzzle-card'
+    name: 'puzzle-card',
+    props: ['string','rules'],
+    data(){
+        return {
+            ruleLegend: ["If you possess a string whose last letter is I, you may add a U to the end", 
+            "Mx (x standing in for any sequence of characters following the M and running to the end of the string), you may add Mxx to your collection.",
+            "If III occurs in the string, you may make a new string with U in place of III",
+            "If UU occurs inside one of your strings, you can drop it"]
+        }
+    },
+    methods: {
+        handleClick(rule){
+            if(rule == 3 ){
+                let x = this.string.indexOf("III");
+                this.$emit('rule', rule, x)
+            }if (rule == 4 ){
+                let x = this.string.indexOf("UU");
+                this.$emit('rule', rule, x)
+            }
+            if(rule == 1 || rule == 2){
+                this.$emit('rule', rule)
+            }
+            
+        }
+    }
 }
 </script>
 
 <style>
 .card {
     /* border: 2px solid rgba(162, 94, 73, 0.5); */
-    width: 200px;
-    height: 50px;
+    width: 250px;
+    height: auto;
     margin: auto;
     vertical-align: bottom;
     background-color: rgb(247, 178, 100);
